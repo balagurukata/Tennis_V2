6ec4f5040033828136aa486aa31c0795beff98f7
2020-DEV-103 <gnagurus@gmail.com>
2020-07-30 17:08:14 +0530

    refactor: added additional loggers to display the player score info and extracted main method

--- src/main/java/com/bnpp/kata/TennisGameRunner.java
+++ src/main/java/com/bnpp/kata/TennisGameRunner.java
@@ -2,28 +2,52 @@ package com.bnpp.kata;
 
 import java.util.Scanner;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;  
+import org.apache.log4j.Logger; 
+
 import com.bnpp.kata.tennis.TennisGame;
 import static com.bnpp.kata.tennis.constant.TennisGameConstants.*;
 import com.bnpp.kata.tennis.exception.InvalidUserInputException;
 
 public class TennisGameRunner {
-
+	
+	private static final Logger LOGGER = LogManager.getLogger(TennisGameRunner.class);
+	
+	public static void main(String[] args) throws InvalidUserInputException {
+		new TennisGameRunner().playGame();
+	}
+	
 	public String playGame() throws InvalidUserInputException {
 		
+		BasicConfigurator.configure(); 
+		
+		LOGGER.info("******** Welcome to play the Tennis Game! ********");
 		String gameScore;
 		
 		Scanner inputScanner = new Scanner(System.in);
+		LOGGER.info("Please enter the first palyer name");
 		String playerOneName = inputScanner.nextLine();
+		LOGGER.info("Please enter the second player name");
 		String playerTwoName = inputScanner.nextLine();
 		TennisGame tennisGame = new TennisGame(playerOneName, playerTwoName);
 		
+		String winningIndicatorMessage = String.format(
+				"Press 'A' to indicate '%s' win the current service or Press 'B' to indicate ' %s' win the current service",
+				playerOneName, playerTwoName);
+
+		LOGGER.info("******** Lets start the game ! ********");
 		do {
+			LOGGER.info(winningIndicatorMessage);
 			char winningIndicator = inputScanner.next().charAt(0);
 			tennisGame.addServicePointToWinner(String.valueOf(winningIndicator));
 			gameScore = tennisGame.computeGameScore();
+			String scoreDetails = String.format("Current Game score: %s", gameScore);
+			LOGGER.info(scoreDetails);
 		} while (isGameInProgress(gameScore));
 
 		inputScanner.close();
+		LOGGER.info("******** Game Over ! ********");
 		return gameScore;
 	}
 
