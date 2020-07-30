dee4a2d82297743fceede10ee93d71c29d107182
2020-DEV-103 <gnagurus@gmail.com>
2020-07-30 14:06:28 +0530

    refactor: extracted the method in tennis game to refining the winning and advantage logic

--- src/main/java/com/bnpp/kata/tennis/TennisGame.java
+++ src/main/java/com/bnpp/kata/tennis/TennisGame.java
@@ -35,10 +35,9 @@ public class TennisGame {
 		
 		if (isPlayersHaveSameScore()) {
 			gameScore = isPalyersHasDeuceScore() ? DEUCE_GAME_SCORE : getGameAllScore();
-		} else if (isPlayerWonTheGame()) {
-			gameScore = getTopScoredPlayerName() + WON_THE_GAME_SCORE_RESULT;
-		} else if (isPlayerHasAdvantageScore()) {
-			gameScore = getTopScoredPlayerName() + ADVANTAGE_GAME_SCORE;
+		} else if (isPlayerHasWinningEligible()) {
+			gameScore = isPlayersHasAdvantageScore() ? getPlayerScore(ADVANTAGE_GAME_SCORE)
+					: getPlayerScore(WON_THE_GAME_SCORE_RESULT);
 		} else {
 			gameScore = getGameScore();
 		}
@@ -46,10 +45,16 @@ public class TennisGame {
 		return gameScore;
 	}
 
-	private boolean isPlayerHasAdvantageScore() {
-		return (firstPlayer.getScoredPoint() >= MINIMUM_WINNING_SCORE
-				|| secondPlayer.getScoredPoint() >= MINIMUM_WINNING_SCORE)
-				&& Math.abs(firstPlayer.getScoredPoint() - secondPlayer.getScoredPoint()) == ADVANTAGE_DIFFERENCE_POINT;
+	private String getPlayerScore(String winningScenario) {
+		return getTopScoredPlayerName() + winningScenario;
+	}
+
+	private boolean isPlayersHasAdvantageScore() {
+		return Math.abs(firstPlayer.getScoredPoint() - secondPlayer.getScoredPoint()) == ADVANTAGE_DIFFERENCE_POINT;
+	}
+
+	private boolean isPlayerHasWinningEligible() {
+		return firstPlayer.getScoredPoint() >= MINIMUM_WINNING_SCORE || secondPlayer.getScoredPoint() >= MINIMUM_WINNING_SCORE;
 	}
 
 	private boolean isPalyersHasDeuceScore() {
@@ -62,15 +67,6 @@ public class TennisGame {
 
 	private String getTopScoredPlayerName() {
 		return firstPlayer.getScoredPoint() > secondPlayer.getScoredPoint() ? firstPlayer.getName() : secondPlayer.getName();
-	}
-	
-	private boolean isPlayerWonTheGame() {
-		return (firstPlayer.getScoredPoint() >= MINIMUM_WINNING_SCORE
-				|| secondPlayer.getScoredPoint() >= MINIMUM_WINNING_SCORE) && isPlayerWinningMinimumDifference();
-	}
-
-	private boolean isPlayerWinningMinimumDifference() {
-		return Math.abs(firstPlayer.getScoredPoint() - secondPlayer.getScoredPoint()) >= MINIMUM_WINNING_DIFFERENCE_POINT;
 	}
 
 	private String getGameScore() {
