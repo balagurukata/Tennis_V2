1bcd4ccd0c905b1655109892b8dee41ad3e22991
2020-DEV-103 <gnagurus@gmail.com>
2020-07-30 15:33:28 +0530

    test: added test suite to execute all test classes

--- src/test/java/com/bnpp/kata/tennis/suite/TennisGameTestSuite.java
+++ src/test/java/com/bnpp/kata/tennis/suite/TennisGameTestSuite.java
@@ -4,11 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.bnpp.kata.TennisGameRunnerTest;
 import com.bnpp.kata.tennis.TennisGameTest;
 import com.bnpp.kata.tennis.model.PlayerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TennisGameTest.class, PlayerTest.class })
+@SuiteClasses({ TennisGameTest.class, PlayerTest.class, TennisGameRunnerTest.class })
 public class TennisGameTestSuite {
 
-} 
+}
