1748b1175289b7e8aa01e3cf3bcf2af81e1b6123
2020-DEV-103 <gnagurus@gmail.com>
2020-07-30 12:52:27 +0530

    refactor: extracted the constants class in tennis game for code readability and maintainability

--- src/main/java/com/bnpp/kata/tennis/TennisGame.java
+++ src/main/java/com/bnpp/kata/tennis/TennisGame.java
@@ -1,17 +1,10 @@
 package com.bnpp.kata.tennis;
 
+import static com.bnpp.kata.tennis.constant.TennisGameConstants.*;
 import com.bnpp.kata.tennis.model.Player;
 
 public class TennisGame {
 	
-	private static final int MINIMUM_WINNING_DIFFERENCE_POINT = 2;
-	private static final int MINIMUM_WINNING_SCORE = 4;
-	private static final String WON_THE_GAME_SCORE_RESULT = " won the game";
-	private static final String GAME_SCORE_ALL = "All";
-	private static final String GAME_SCORE_SEPARATOR = "-";
-	private static final String[] GAME_SCORE = { "Love", "Fifteen", "Thirty", "Forty" };
-	private static final String SECOND_PLAYER_NAME = "SecondPlayer";
-	private static final String FIRST_PLAYER_NAME = "FirstPlayer";
 	private Player firstPlayer;
 	private Player secondPlayer;
 
---
+++ src/main/java/com/bnpp/kata/tennis/constant/TennisGameConstants.java
@@ -0,0 +1,16 @@
+package com.bnpp.kata.tennis.constant;
+
+public class TennisGameConstants {
+
+	private TennisGameConstants() {
+	}
+
+	public static final int MINIMUM_WINNING_DIFFERENCE_POINT = 2;
+	public static final int MINIMUM_WINNING_SCORE = 4;
+	public static final String WON_THE_GAME_SCORE_RESULT = " won the game";
+	public static final String GAME_SCORE_ALL = "All";
+	public static final String GAME_SCORE_SEPARATOR = "-";
+	public static final String[] GAME_SCORE = { "Love", "Fifteen", "Thirty", "Forty" };
+	public static final String SECOND_PLAYER_NAME = "SecondPlayer";
+	public static final String FIRST_PLAYER_NAME = "FirstPlayer";
+}
